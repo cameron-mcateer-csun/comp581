1cdbc8a168a406cf7e718e4421f402f86997fe22
Cameron McAteer <cameron.mcateer.424@my.csun.edu>
2013-05-09 11:16:38 -0700

    beginning changes to the JS module for chatting

--- tomcat/webapps/ROOT/includes/js/chat.js
+++ tomcat/webapps/ROOT/includes/js/chat.js
@@ -16,53 +16,123 @@
  * limitations under the License.
  */
 var client, destination;
-$(document).ready(function(){
+function onDocuReady() {
+	$('#connect_form').submit(function() {
+		var url = "ws://" + window.location.host + ":61616";
+		var login = $("#connect_login").val();
+		var passcode = $("#connect_passcode").val();
+		destination = login;
 
+		client = Stomp.client(url);
 
-  $('#connect_form').submit(function() {
-    var url = "ws://" + window.location.host + ":61616";
-    var login = $("#connect_login").val();
-    var passcode = $("#connect_passcode").val();
-    destination = $("#destination").val();
+		// this allows to display debug logs directly on the web page
+		client.debug = function(str) {
+			$("#debug").append(str + "\n");
+		};
 
-    client = Stomp.client(url);
+		// the client is notified when it is connected to the server.
+		var onconnect = function(frame) {
+			client.debug("connected to Stomp");
+			$('#connect').fadeOut({
+				duration: 'fast'
+			});
+			$('#disconnect').fadeIn();
+			$('#send_form_input').removeAttr('disabled');
 
-    // this allows to display debug logs directly on the web page
-    client.debug = function(str) {
-      $("#debug").append(str + "\n");
-    };
-    // the client is notified when it is connected to the server.
-    var onconnect = function(frame) {
-      client.debug("connected to Stomp");
-      $('#connect').fadeOut({ duration: 'fast' });
-      $('#disconnect').fadeIn();
-      $('#send_form_input').removeAttr('disabled');
+			client.subscribe(destination, function(message) {
+				$("#messages").append("<p>" + message.body + "</p>\n");
+			});
+		};
 
-      client.subscribe(destination, function(message) {
-        $("#messages").append("<p>" + message.body + "</p>\n");
-      });
-    };
-    client.connect(login, passcode, onconnect);
+		client.connect(login, passcode, onconnect);
 
-    return false;
-  });
+		return false;
+	});
 
-  $('#disconnect_form').submit(function() {
-    client.disconnect(function() {
-      $('#disconnect').fadeOut({ duration: 'fast' });
-      $('#connect').fadeIn();
-      $('#send_form_input').addAttr('disabled');
-    });
-    return false;
-  });
+	$('#disconnect_form').submit(function() {
+		client.disconnect(function() {
+			$('#disconnect').fadeOut({
+				duration: 'fast'
+			});
+			$('#connect').fadeIn();
+			$('#send_form_input').addAttr('disabled');
+		});
+		return false;
+	});
 
-  $('#send_form').submit(function() {
-    var text = $('#send_form_input').val();
-    if (text) {
-      client.send(destination, {foo: 1}, text);
-      $('#send_form_input').val("");
-    }
-    return false;
-  });
+	$('#send_form').submit(function() {
+		var text = $('#send_form_input').val();
+		if (text) {
+			client.send(destination, {}, text);
+			$('#send_form_input').val("");
+		}
+		return false;
+	});
 
-});
+}
+
+(function() {
+	this.Chat = {
+		client: {},
+
+		connect: function(login, passcode, callback, errorcallback) {
+			if (this.client.connected)
+				return;
+
+			var url = "ws://" + window.location.host + ":61616";
+
+			this.client = Stomp.client(url);
+
+			// this allows to display debug logs directly on the web page
+			this.client.debug = function(str) {
+				$("#debug").append(str + "\n");
+			};
+
+			// the client is notified when it is connected to the server.
+			var onconnect = function(frame) {
+				this.client.debug("connected to Stomp");
+
+				if (callback instanceof Function)
+					callback();
+			};
+
+			this.client.connect(login, passcode, onconnect, errorcallback);
+
+			return false;
+		} // connect,
+
+		disconnect: function(callback) {
+			if (!this.client.connected)
+				return;
+			$(this.client.subscriptions).each(function(id, subscription) {
+				this.client.unsubscribe(id);
+			})
+			this.client.disconnect(callback);
+			return false;
+		},
+
+		send: function(text) {
+			if (!this.client.connected)
+				return;
+			if (text) {
+				this.client.send(destination, {}, text);
+			}
+			return false;
+		},
+
+		subscribe: function(destination, callback) {
+			if (!this.client.connected)
+				return;
+
+			var _callback = function(msg) {
+				callback(destination.replace(/^\/topic\//i, ""), msg);
+			}
+
+			this.client.subscribe("/topic/"+destination, _callback);
+		},
+
+		unsubscribe: function(id) {
+			this.client.unsubscribe(id);
+		}
+	}
+}).call(this)
