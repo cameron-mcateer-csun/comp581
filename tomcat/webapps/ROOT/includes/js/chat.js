f4c0e75e2c7b63104a6349b994020b50ce6988e3
Cameron McAteer <cameron.mcateer.424@my.csun.edu>
2013-05-09 13:41:04 -0700

    changed chat app to operate more like twitter

--- tomcat/webapps/ROOT/includes/js/chat.js
+++ tomcat/webapps/ROOT/includes/js/chat.js
@@ -1,25 +1,24 @@
-/**
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the 'License'); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 var client, destination;
 function onDocuReady() {
+	function makeOnMessage(topic_name) {
+		return function(message) {
+			$('#messages').append("<p>" + "<strong><br>" + topic_name + ":</strong>&nbsp;" + message.body + "</p>\n");
+		}
+	}
+
+	function validTopicName(topic_name) {
+		return /^[a-zA-z0-9_]+$/.test(topic_name);
+	}
+
 	$('#connect_form').submit(function() {
-		var url = "ws://" + window.location.host + ":61616";
+		// check here for local testing
+		var host = /file/i.test(window.location.protocol) ? "ec2-75-101-218-0.compute-1.amazonaws.com" : window.location.host;
+		var url = "ws://" + host + ":61616";
 		var login = $("#connect_login").val();
+		if (!validTopicName(login)) {
+			alert("Cannot login with this name");
+			return false;
+		}
 		var passcode = $("#connect_passcode").val();
 		destination = login;
 
@@ -37,25 +36,44 @@ function onDocuReady() {
 				duration: 'fast'
 			});
 			$('#disconnect').fadeIn();
+			$('#subscribe').fadeIn();
 			$('#send_form_input').removeAttr('disabled');
-
-			client.subscribe(destination, function(message) {
-				$("#messages").append("<p>" + message.body + "</p>\n");
-			});
 		};
 
-		client.connect(login, passcode, onconnect);
+		client.connect("/topic/"+login, passcode, onconnect);
+
+		return false;
+	});
+
+	$('#subscribe_form').submit(function() {
+		var uname = $('#subscribe_input').val();
+
+		if (!validTopicName(uname)) {
+			alert("Cannot subcsribe to this topic");
+			return false;
+		}
+
+		client.subscribe("/topic/"+uname, makeOnMessage(uname));
+
+		$('#subscribe_input').val("");
 
 		return false;
 	});
 
 	$('#disconnect_form').submit(function() {
+		for (var id in client.subscriptions) {
+			client.unsubscribe(id);
+		}
 		client.disconnect(function() {
 			$('#disconnect').fadeOut({
 				duration: 'fast'
 			});
+			$('#subscribe').fadeOut({
+				duration: 'fast'
+			});
 			$('#connect').fadeIn();
-			$('#send_form_input').addAttr('disabled');
+			$('#send_form_input').attr('disabled', 'true');
+			$('#messages').html("");
 		});
 		return false;
 	});
@@ -63,7 +81,7 @@ function onDocuReady() {
 	$('#send_form').submit(function() {
 		var text = $('#send_form_input').val();
 		if (text) {
-			client.send(destination, {}, text);
+			client.send("/topic/"+destination, {}, text);
 			$('#send_form_input').val("");
 		}
 		return false;
@@ -71,68 +89,4 @@ function onDocuReady() {
 
 }
 
-(function() {
-	this.Chat = {
-		client: {},
-
-		connect: function(login, passcode, callback, errorcallback) {
-			if (this.client.connected)
-				return;
-
-			var url = "ws://" + window.location.host + ":61616";
-
-			this.client = Stomp.client(url);
-
-			// this allows to display debug logs directly on the web page
-			this.client.debug = function(str) {
-				$("#debug").append(str + "\n");
-			};
-
-			// the client is notified when it is connected to the server.
-			var onconnect = function(frame) {
-				this.client.debug("connected to Stomp");
-
-				if (callback instanceof Function)
-					callback();
-			};
-
-			this.client.connect(login, passcode, onconnect, errorcallback);
-
-			return false;
-		} // connect,
-
-		disconnect: function(callback) {
-			if (!this.client.connected)
-				return;
-			$(this.client.subscriptions).each(function(id, subscription) {
-				this.client.unsubscribe(id);
-			})
-			this.client.disconnect(callback);
-			return false;
-		},
-
-		send: function(text) {
-			if (!this.client.connected)
-				return;
-			if (text) {
-				this.client.send(destination, {}, text);
-			}
-			return false;
-		},
-
-		subscribe: function(destination, callback) {
-			if (!this.client.connected)
-				return;
-
-			var _callback = function(msg) {
-				callback(destination.replace(/^\/topic\//i, ""), msg);
-			}
-
-			this.client.subscribe("/topic/"+destination, _callback);
-		},
-
-		unsubscribe: function(id) {
-			this.client.unsubscribe(id);
-		}
-	}
-}).call(this)
+$(document).ready(onDocuReady);
